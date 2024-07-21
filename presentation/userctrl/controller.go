cd240dd945d7644ae88b6b4649798070c6dcf9b1
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-07-20 22:51:13 -0500

    feat: get user by id

--- presentation/userctrl/controller.go
+++ presentation/userctrl/controller.go
@@ -4,6 +4,7 @@ import (
 	"paywatcher/domain/userdomain"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type UserController struct {
@@ -16,10 +17,21 @@ func NewUserController(repo userdomain.UserRepository) *UserController {
 	}
 }
 
-// func (c *UserController) GetUser(ctx *fiber.Ctx) error {
-// 	id := ctx.Params("id")
+func (c *UserController) GetUserById(ctx *fiber.Ctx) error {
+	idString := ctx.Params("id")
+	id, err := uuid.Parse(idString)
 
-// }
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "invalid id"})
+	}
+
+	user, err := c.Repo.GetUserById(id)
+	if err != nil {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": "error", "message": "invalid id"})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"status": "ok", "data": user})
+}
 
 func (c UserController) Index(ctx *fiber.Ctx) error {
 	return ctx.SendString("Hello, World!!")
