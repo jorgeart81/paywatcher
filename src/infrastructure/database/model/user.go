adfc88f69d352dd73acd60c392fdd901940990ea
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-07-22 12:43:53 -0500

    fix: save role in db

--- src/infrastructure/database/model/user.go
+++ src/infrastructure/database/model/user.go
@@ -12,6 +12,7 @@ type User struct {
 	Email     string    `gorm:"column:email;unique"`
 	Username  string    `gorm:"column:username"`
 	Password  string    `gorm:"column:password"`
+	Role      []string  `gorm:"column:role;serializer:json"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
@@ -22,6 +23,7 @@ func ToUserEntity(user *userdomain.User) *User {
 		Username: user.Username,
 		Email:    user.Email,
 		Password: user.Password,
+		Role:     ToStringRoles(user.Role),
 	}
 }
 
@@ -31,5 +33,22 @@ func (e *User) ToDomain() *userdomain.User {
 		Username: e.Username,
 		Email:    e.Email,
 		Password: e.Password,
+		Role:     ToDomainRoles(e.Role),
 	}
+}
+
+func ToDomainRoles(roles []string) []userdomain.Role {
+	domainRoles := make([]userdomain.Role, len(roles))
+	for i, role := range roles {
+		domainRoles[i] = userdomain.Role(role)
+	}
+	return domainRoles
+}
+
+func ToStringRoles(roles []userdomain.Role) []string {
+	stringRoles := make([]string, len(roles))
+	for i, role := range roles {
+		stringRoles[i] = string(role)
+	}
+	return stringRoles
 }
