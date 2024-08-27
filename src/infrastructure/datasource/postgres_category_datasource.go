3cd8c26d819a37ed854105c7e1cc5ccafa93d74f
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-08-26 19:24:22 -0500

    feat: Add method to save category in data source

--- src/infrastructure/datasource/postgres_category_datasource.go
+++ src/infrastructure/datasource/postgres_category_datasource.go
@@ -1,8 +1,12 @@
 package datasource
 
 import (
+	"errors"
+	"fmt"
 	"paywatcher/src/domain/entity"
+	"paywatcher/src/infrastructure/database/schemas"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -13,6 +17,20 @@ type PostgresCategoryDatasrc struct {
 }
 
 // Save implements datasource.CategoryDS.
-func (p *PostgresCategoryDatasrc) Save(user entity.CategoryEnt) (*entity.CategoryEnt, error) {
-	panic("unimplemented")
+func (p *PostgresCategoryDatasrc) Save(category entity.CategoryEnt, userID uuid.UUID) (*entity.CategoryEnt, error) {
+	db := p.DB
+	categorySchema := schemas.ToCategorySchema(&category)
+
+	var user schemas.User
+	db.Preload("Categories").First(&user, userID)
+
+	user.Categories = append(user.Categories, *categorySchema)
+
+	if err := db.Save(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRegistered) {
+			return nil, fmt.Errorf("category could not be created")
+		}
+		return nil, err
+	}
+	return categorySchema.ToDomain(), nil
 }
